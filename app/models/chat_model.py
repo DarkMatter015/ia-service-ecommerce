3924337a566066514fbcb7d87b17af307a1cc581
DarkMatter015 <decamargoluk@gmail.com>
2026-02-15 17:43:01 -0300

    refactor: ajusting the model of chat tables

--- app/models/chat_model.py
+++ app/models/chat_model.py
@@ -2,15 +2,25 @@ from sqlalchemy import Column, String, Integer, ForeignKey, Text, DateTime
 from sqlalchemy.orm import relationship
 from app.core.database import Base
 import datetime
+from sqlalchemy.dialects.postgresql import UUID, BigInteger
+import uuid
+import timezone
 
 
 class ChatSession(Base):
     __tablename__ = "chat_sessions"
 
-    id = Column(String, primary_key=True)  # UUID gerado no Front ou Back
-    user_id = Column(Integer, index=True)
-    title = Column(String, nullable=True)  # Ex: "Dúvida sobre Guitarra"
-    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    # Usamos UUID nativo para performance e segurança
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    # Alinhado com o BIGINT do Java/Postgres
+    user_id = Column(BigInteger, nullable=False, index=True)
+    title = Column(String(255), nullable=True)
+
+    # timezone=True garante compatibilidade com TIMESTAMPTZ
+    created_at = Column(
+        DateTime(timezone=True), default=lambda: datetime.now(timezone.utc)
+    )
+
     messages = relationship(
         "ChatMessage", back_populates="session", cascade="all, delete-orphan"
     )
@@ -19,9 +29,16 @@ class ChatSession(Base):
 class ChatMessage(Base):
     __tablename__ = "chat_messages"
 
-    id = Column(Integer, primary_key=True)
-    session_id = Column(String, ForeignKey("chat_sessions.id"))
-    role = Column(String)  # 'user' ou 'assistant'
-    content = Column(Text)
-    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    # BIGSERIAL no Postgres mapeia para BigInteger no SQLAlchemy
+    id = Column(BigInteger, primary_key=True, autoincrement=True)
+    session_id = Column(
+        UUID(as_uuid=True), ForeignKey("chat_sessions.id"), nullable=False
+    )
+
+    role = Column(String(20), nullable=False)  # 'user', 'assistant', 'system'
+    content = Column(Text, nullable=False)
+    created_at = Column(
+        DateTime(timezone=True), default=lambda: datetime.now(timezone.utc)
+    )
+
     session = relationship("ChatSession", back_populates="messages")
